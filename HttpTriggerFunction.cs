73a7a03aaf240e6c5ada50180c59ffefa7d47fc5
eelahi <eelahi@ael-bd.com>
2024-03-11 18:34:16 +0600

    Add project files.

---
+++ HttpTriggerFunction.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace MyFunctionApp
+{
+    public class HttpTriggerFunction
+    {
+        private readonly ILogger<HttpTriggerFunction> _logger;
+
+        public HttpTriggerFunction(ILogger<HttpTriggerFunction> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("HttpTriggerFunction")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult($"Welcome to Azure Functions {req.Query["name"]}!");
+        }
+    }
+}
